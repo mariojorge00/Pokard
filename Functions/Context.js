39a7a7142b0bcc75f4dbece15bef75160978a31c
mariojorge00 <102314629+mariojorge00@users.noreply.github.com>
2022-05-27 21:27:13 +0200

    Delete Functions directory

--- Functions/Context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const weakTypescontext = createContext();
-export const typeSelectcontext = createContext();
-export const hiddencontext = createContext({
-  hidden: "visible",
-  setHidden: () => {},
-});
-export const pokemonscontext = createContext();
--- Functions/Standarized.js
+++
@@ -1,7 +0,0 @@
-export default function Standarized(info) {
-  if (!info || info.length === 0) return 0;
-
-  if (info.id < 10) return `#00${info.id}`;
-  if (info.id < 100) return `#0${info.id}`;
-  else return `#${info.id}`;
-}
